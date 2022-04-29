c7cdbc5c25a786b9a636ac25d5ca15defbc33c43
LuisAlfredo <alfredoavendano@hotrmail.com>
2022-04-29 19:54:39 +0100

    global_step

--- multiprocesing_test.py
+++ multiprocesing_test.py
@@ -265,7 +265,7 @@ class CIFAR10ModelSAM(pl.LightningModule):
             self.mask.apply_mask()
             one_forward_backward_pass = self.sparse_inference_flops * 2
             self.training_flops += one_forward_backward_pass * 2
-            self.log("epoch_flops", self.training_flops, on_step=false, on_epoch=true)
+            self.log("epoch_flops", self.training_flops, on_step=False, on_epoch=True)
 
         # flops for one forward pass
         # assuming that the backward pass uses approximately the same number of flops as the
@@ -634,8 +634,9 @@ def manual_SGD_optimization(cfg: omegaconf.DictConfig):
     return 0
 
 
-def train_SAM(model: nn.Module,mask:Masking ,optimizer: torch.optim.Optimizer, device: torch.device, train_loader: DataLoader,
-              loss_object: typing.Callable, epoch: int, global_step: int, train_flops:float,log_interval: int = 100,
+def train_SAM(model: nn.Module, mask: Masking, optimizer: torch.optim.Optimizer, device: torch.device,
+              train_loader: DataLoader,
+              loss_object: typing.Callable, epoch: int, global_step: int, train_flops: float, log_interval: int = 100,
               use_wandb: bool = False):
     model.train()
     _mask_update_counter = 0
@@ -659,7 +660,7 @@ def train_SAM(model: nn.Module,mask:Masking ,optimizer: torch.optim.Optimizer, d
         optimizer.second_step(zero_grad=True)
         if mask:
             mask.apply_mask()
-            one_forward_backward_pass = mask.inference_FLOPs* 2
+            one_forward_backward_pass = mask.inference_FLOPs * 2
             train_flops += one_forward_backward_pass * 2
         # L2 Regularization
 
@@ -688,7 +689,7 @@ def train_SAM(model: nn.Module,mask:Masking ,optimizer: torch.optim.Optimizer, d
             msg = f"Train Epoch {epoch} Iters {global_step} Train loss {_loss_collector.smooth:.6f}"
             pbar.set_description(msg)
 
-            if use_wandb :
+            if use_wandb:
                 log_dict = {"train_loss": loss}
                 # if mask:
                 #     density = mask.stats.total_density
@@ -704,7 +705,7 @@ def train_SAM(model: nn.Module,mask:Masking ,optimizer: torch.optim.Optimizer, d
 
 
 def evaluate(model: nn.Module, valLoader: DataLoader, device: torch.device, loss_object: typing.Callable,
-             epoch: int, is_test_set: bool = False, use_wandb: bool = False):
+             epoch: int, global_step:int,is_test_set: bool = False, use_wandb: bool = False):
     model.eval()
     top1_list = []
     top5_list = []
@@ -736,9 +737,9 @@ def evaluate(model: nn.Module, valLoader: DataLoader, device: torch.device, loss
     logging.info(msg)
 
     if use_wandb:
-        wandb.log({f"{val_or_test}_loss": loss}, epoch=epoch)
-        wandb.log({f"{val_or_test}_accuracy": top_1_accuracy}, epoch=epoch)
-        wandb.log({f"{val_or_test}_top_5_accuracy": top_5_accuracy}, epoch=epoch)
+        wandb.log({f"{val_or_test}_loss": loss}, step=global_step)
+        wandb.log({f"{val_or_test}_accuracy": top_1_accuracy}, step=global_step)
+        wandb.log({f"{val_or_test}_top_5_accuracy": top_5_accuracy}, step=global_step)
 
 
 def enable_bn(model):
@@ -780,7 +781,6 @@ def manual_SAM_optimization(cfg: omegaconf.DictConfig):
     weight_decay = cfg.weight_decay
     training_FLOPS: float = 0
 
-
     if cfg.weight_decay:
         parameters = train_helper._add_weight_decay(model, weight_decay)
         weight_decay = 0
@@ -790,7 +790,6 @@ def manual_SAM_optimization(cfg: omegaconf.DictConfig):
     optimizer = SAM(params=parameters, base_optimizer=torch.optim.SGD, lr=cfg.learning_rate,
                     rho=cfg.rho,
                     adaptive=cfg.adaptive, momentum=cfg.momentum, weight_decay=weight_decay)
-
 
     lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer.base_optimizer, cfg.epochs)
     if cfg.wandb:
@@ -814,9 +813,9 @@ def manual_SAM_optimization(cfg: omegaconf.DictConfig):
 
     for epoch in range(cfg.epochs):
 
-        train_SAM(model,mask, optimizer, device, train_loader, loss_object, epoch, global_step,
-                  use_wandb=cfg.wandb,train_flops=training_FLOPS)
-        wandb.log({"train_FLOPS": training_FLOPS},step=global_step)
+        train_SAM(model, mask, optimizer, device, train_loader, loss_object, epoch, global_step,
+                  use_wandb=cfg.wandb, train_flops=training_FLOPS)
+        wandb.log({"train_FLOPS": training_FLOPS}, step=global_step)
         lr_scheduler.step()
 
         if epoch % cfg.val_interval == 0:
@@ -826,6 +825,7 @@ def manual_SAM_optimization(cfg: omegaconf.DictConfig):
                 device,
                 loss_object,
                 epoch,
+                global_step,
                 use_wandb=cfg.wandb
             )
 
